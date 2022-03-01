db725a0a7a3bb30f88d12b49941ebe596af1a1b2
Marian Wang <mw@bubs-mbp.lan>
2022-03-01 12:08:25 -0500

    outer petal color

--- js/flower_gen_2.js
+++ js/flower_gen_2.js
@@ -1,37 +1,17 @@
-// taken from https://github.com/macklab/macklab.github.io/blob/master/flowers/js/flower_generator.js
-
 paper.view.viewSize.width = 400;
 paper.view.viewSize.height = 400;
 paper.view.autoUpdate = true;
 
 //context = canvas.getContext('2d');
 window.globals = {};
-//var scope = this;
+var scope = this;
 
-function drawFlower1(petalColor, petalShape, circleShape) {
-  //scope.activate();
+function drawFlower1(petalColor, petalShape, ringShape, circleColor) {
+  scope.activate();
   project.activeLayer.removeChildren();
 
   var xoff = 200;
   var yoff = 200;
-
-  ringShape = 4.5;
-  circleColor = 4.5;
-  petal_shape_max = 11;
-
-  // calculate color
-  //alert(petalColor);
-  var a_val = 80 * Math.cos((petalColor * Math.PI) / 180);
-  var b_val = 80 * Math.sin((petalColor * Math.PI) / 180);
-  var l_val = 60;
-  //alert([l_val, a_val, b_val])
-  var rgb_color = xyzToRgb(labToXyz([l_val, a_val, b_val]));
-  //alert(rgb_color);
-
-  pcolor = petalColor;
-  petalRatio = petalShape / petal_shape_max;
-  npetals = Math.round(10 + petalRatio * petal_shape_max);
-  petalSize1 = [100, 62 - 46 * petalRatio];
 
   // green leaves
   nleaves = 50;
@@ -48,6 +28,12 @@ function drawFlower1(petalColor, petalShape, circleShape) {
     leaf.flatten(5);
   }
 
+  colorRatio = petalColor / 9;
+  pcolor = 325 + colorRatio * 10;
+  petalRatio = petalShape / 9;
+  npetals = Math.round(11 + petalRatio * 9);
+  petalSize1 = [100, 30 - 15 * petalRatio];
+  petalSize2 = [75, 15 - 7 * petalRatio];
   // larger petals
   for (var x = 0; x < npetals; x++) {
     var ang = x * (360 / npetals) + (6 * Math.random() - 3);
@@ -73,11 +59,9 @@ function drawFlower1(petalColor, petalShape, circleShape) {
   }
 
   // smaller petals
-  petalSize2 = [75, 6.15 + 1.85 * petalShape];
-  nspetals = Math.round(5 + petalShape * petal_shape_max);
-  for (var x = 0; x < nspetals; x++) {
+  for (var x = 0; x < npetals; x++) {
     var ang =
-      x * (360 / nspetals) + 360 / (nspetals * 2) + (6 * Math.random() - 3);
+      x * (360 / npetals) + 360 / (npetals * 2) + (6 * Math.random() - 3);
     var cx = xoff + 25 * Math.cos((ang * Math.PI) / 180);
     var cy = yoff + 25 * Math.sin((ang * Math.PI) / 180);
     var petal = new Path.Ellipse({
@@ -89,14 +73,8 @@ function drawFlower1(petalColor, petalShape, circleShape) {
       shadowBlur: 8,
       shadowOffset: new Point(0, 0),
     });
-    if (pcolor == 0) {
-      petal.fillColor.saturation = 0;
-      petal.fillColor.brightness = 50;
-    } else {
-      petal.fillColor.red = rgb_color[0];
-      petal.fillColor.green = rgb_color[1];
-      petal.fillColor.blue = rgb_color[2];
-    }
+    petal.fillColor.hue = pcolor + 5 * Math.random() - 2.5;
+    petal.fillColor.brightness -= 0.1 * Math.random() - 0.05;
     new Path.Ellipse({
       center: [cx, cy],
       size: [50, 2],
@@ -108,14 +86,14 @@ function drawFlower1(petalColor, petalShape, circleShape) {
   // inner ring
   var ringRatio = ringShape / 9;
   var nring = Math.round(15 + 30 * ringRatio);
-  var ringSize = [35, 15 - 12 * ringRatio];
+  var ringSize = [12 + 11 * ringRatio, 15 - 12 * ringRatio];
   var rcolor = new Color(225 / 255, 229 / 255, 20 / 255, 0.95);
   for (var x = 0; x < nring; x++) {
     var ang = x * (360 / nring);
     var cx =
-      xoff + 15 * Math.cos((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
+      xoff + 20 * Math.cos((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
     var cy =
-      yoff + 15 * Math.sin((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
+      yoff + 20 * Math.sin((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
     var ring = new Path.Ellipse({
       center: [cx, cy],
       size: ringSize,
@@ -125,22 +103,16 @@ function drawFlower1(petalColor, petalShape, circleShape) {
   }
 
   // inner circle
-  var circleSize = 3.3 + (21.7 * circleShape) / 13;
-
-  var circleRatio = circleColor / 13;
+  var circleRatio = circleColor / 9;
   var icircle = new Path();
   icircle.fillColor = new Color(104 / 255, 50 / 255, 14 / 255);
   icircle.fillColor.hue += circleRatio * 35;
   for (var x = 0; x < 100; x++) {
     var ang = x * (360 / 60);
     var cx =
-      xoff +
-      circleSize * Math.cos((ang * Math.PI) / 180) +
-      (1 * Math.random() - 0.5);
+      xoff + 18 * Math.cos((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
     var cy =
-      yoff +
-      circleSize * Math.sin((ang * Math.PI) / 180) +
-      (1 * Math.random() - 0.5);
+      yoff + 18 * Math.sin((ang * Math.PI) / 180) + (1 * Math.random() - 0.5);
     icircle.add(new Point(cx, cy));
   }
   icircle.closePath();
@@ -165,19 +137,39 @@ function drawFlower1(petalColor, petalShape, circleShape) {
       spot.fillColor.alpha = 0.2;
     }
   }
-
-  paper.view.draw();
-  //paper.view.element.toBlob(function(blob) { saveAs(blob, "image.png");});
+  /*for (var x=0;x<200;x++){
+        rang = 2*Math.random()*Math.PI;
+        rrad = 15*Math.random();
+        var cx = xoff+rrad*Math.cos(rang);
+        var cy = yoff+rrad*Math.sin(rang);
+        var spot = new Path.Ellipse({
+            center: [cx,cy],
+            size: [3,3],
+            fillColor: icircle.fillColor
+        });
+        spot.fillColor.hue += 5*Math.random()-2.5;
+        spot.fillColor.brightness += 0.3*Math.random();//(1-(rrad/15));
+        spot.fillColor.alpha = 0.2;
+    }*/
 }
 
-function download_img(petalColor, petalShape, circleShape) {
+function download_img(petalColor, petalShape, ringShape, circleColor) {
   var link = document.createElement("a");
-  link.href = paper.view.element.toDataURL();
+  link.href = canvas.toDataURL();
   link.download =
-    "flower1_c" + petalColor + "s" + petalShape + circleShape + ".png";
+    "flower1_" + petalColor + petalShape + ringShape + circleColor + ".png";
   link.click();
 }
 
-drawFlower1(1, 1, 1);
+/*
+drawFlower1(5,1,1,1);
+view.update();
+download_img(5,1,1,1,1);
+project.activeLayer.removeChildren();
+drawFlower1(5,9,9,9);
+view.update();
+download_img(5,9,9,9,1);
+*/
+
+//drawFlower1(1,1,1,1);
 globals.drawFlower1 = drawFlower1;
-globals.download_img = download_img;
